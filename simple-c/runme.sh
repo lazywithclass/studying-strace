bc369da69175d5b56906c61ad2c4dafc56707208
lazywithclass <montecristoh@gmail.com>
2015-01-02 21:23:53 +0000

    added shabang

--- simple-c/runme.sh
+++ simple-c/runme.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # we also passed the -s flag to strace telling it the maximum string size
 # we want it to print. this is helpful for showing expanded function arguments.
 # here we pass 2000, which is abitrarily enough to see everything we need to see in this program
